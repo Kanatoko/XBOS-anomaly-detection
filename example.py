8b44d8b3114a0bbdff8f2fa86684ece01d775cb0
kanatoko <github@st.bitforest.jp>
2018-03-04 12:21:58 +0900

    first commit

---
+++ example.py
@@ -0,0 +1,8 @@
+from pandas import DataFrame
+from xbos import XBOS
+
+data = DataFrame(data={'col2':[1,1,1,1,2,2,2,2,2,2,2,2,3,5,5,6,6,7,7,7,7,7,7,7,15],'col1':[1,1,1,1,2,2,2,2,2,2,2,2,3,5,5,6,6,7,7,7,13,13,13,14,15]})
+xbos = XBOS(n_clusters=3)
+result = xbos.fit_predict(data)
+for i in result:
+    print(round(i,2))
---
+++ xbos.py
@@ -0,0 +1,55 @@
+from sklearn.cluster import KMeans
+import numpy as np
+from pandas import DataFrame
+from math import pow
+import math
+
+class XBOS:
+    
+    def __init__(self,n_clusters=15,effectiveness=500,max_iter=2):
+        self.n_clusters=n_clusters
+        self.effectiveness=effectiveness
+        self.max_iter=max_iter
+        self.kmeans = {}
+        self.cluster_score = {}
+        
+    def fit(self, data):
+        length = len(data)
+        for column in data.columns:
+            kmeans = KMeans(n_clusters=self.n_clusters,max_iter=self.max_iter)
+            self.kmeans[column]=kmeans
+            kmeans.fit(data[column].values.reshape(-1,1))
+            assign = DataFrame(kmeans.predict(data[column].values.reshape(-1,1)),columns=['cluster'])
+            cluster_score=assign.groupby('cluster').apply(len).apply(lambda x:x/length)
+            ratio=cluster_score.copy()
+        
+            sorted_centers = sorted(kmeans.cluster_centers_)
+            max_distance = ( sorted_centers[-1] - sorted_centers[0] )[ 0 ]
+        
+            for i in range(self.n_clusters):
+                for k in range(self.n_clusters):
+                    if i != k:
+                        dist = abs(kmeans.cluster_centers_[i] - kmeans.cluster_centers_[k])/max_distance
+                        effect = ratio[k]*(1/pow(self.effectiveness,dist))
+                        cluster_score[i] = cluster_score[i]+effect
+                        
+            self.cluster_score[column] = cluster_score
+                    
+    def predict(self, data):
+        length = len(data)
+        score_array = np.zeros(length)
+        for column in data.columns:
+            kmeans = self.kmeans[ column ]
+            cluster_score = self.cluster_score[ column ]
+            
+            assign = kmeans.predict( data[ column ].values.reshape(-1,1) )
+            #print(assign)
+            
+            for i in range(length):
+                score_array[i] = score_array[i] + math.log10( cluster_score[assign[i]] )
+            
+        return score_array
+    
+    def fit_predict(self,data):
+        self.fit(data)
+        return self.predict(data)
